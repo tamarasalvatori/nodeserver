779b5f04195485517d39b654b21e7761fdef558c
Tamara Salvatori <tamarasalvatori@gmail.com>
2022-03-10 16:03:28 -0300

    POST method working

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require ('express')
 const app = express()
 
-const lista_produtos = {
-    produtos: [
+const lista_produtos = 
+     [
         { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
         { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
         { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
         { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
         { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
     ]
-}
 
-//const Product = require('./models/Product')
 
 app.use (express.urlencoded({extended: true})) //processa o body em formato URLEnconded
 app.use (express.json()) //Processa o body em formato JSON
@@ -40,26 +38,26 @@ app.get ('/api/produtos/:id', (req, res, next) => {
 app.post ('/api/produtos/', (req, res, next) => {
     const { id, descricao, valor, marca } = req.body
 
-    lista_produtos.push(id, descricao, valor, marca)
-
-    return res.json(lista_produtos)
-
-    // try {
-    //     await Product.create(product)
-    //     res.status(201).json({ message: 'Produto inserido na lista com sucesso!' })
-    // } catch {
-    //     res.status(500).json({erro: error})    
-    // }
-
-    //res.json( { message: 'A implementar' } )
+    try {
+        lista_produtos.push({ id, descricao, valor, marca})
+        return res.json(lista_produtos)
+        res.status(201).json({ message: 'Produto inserido na lista com sucesso!' })
+    } catch {
+        res.status(500).json({erro: error})    
+    }
 })
 
 app.put ('/api/produtos/:id', (req, res, next) => {
+    let id = parseInt (req.params.id)
+    let idx = lista_produtos.produtos.findIndex (elem => elem.id === id)
 
     res.json( { message: 'A implementar' } )
 })
 
 app.delete ('/api/produtos/:id', (req, res, next) => {
+    let id = parseInt (req.params.id)
+    let idx = lista_produtos.produtos.findIndex (elem => elem.id === id)
+
     res.json( { message: 'A implementar' } )
 })
 
--- models/Product.js
+++
@@ -1,8 +0,0 @@
-const Product = model('Product', {
-    id: Number,
-    descricao: String,
-    valor: Number,
-    marca: String,
-})
-
-module.exports = Product
