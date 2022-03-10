8b9c47f6cd95444c18be3eceed77e524e7da5577
Tamara Salvatori <tamarasalvatori@gmail.com>
2022-03-10 19:09:54 -0300

    Right JSON

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require ('express')
 const app = express()
 
 const lista_produtos = 
-     [
+     {produtos: [
         { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
         { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
         { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
         { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
         { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
-    ]
+    ]}
 
 
 app.use (express.urlencoded({extended: true})) //processa o body em formato URLEnconded
@@ -39,7 +39,8 @@ app.post ('/api/produtos/', (req, res, next) => {
     const { id, descricao, valor, marca } = req.body
 
     try {
-        lista_produtos.push({ id, descricao, valor, marca})
+        //lista_produtos.push({ id, descricao, valor, marca})
+        lista_produtos.push([{ id, descricao, valor, marca}])
         return res.json(lista_produtos)
         res.status(201).json({ message: 'Produto inserido na lista com sucesso!' })
     } catch {
